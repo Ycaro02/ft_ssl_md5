e49e7e43b3c94eae8b06da2ade4aadf1699f4831
Ycaro02 <Ycaro02@hotmail.fr>
2024-07-08 12:42:20 +0200

    Work on contextual info handling

--- src/main.c
+++ src/main.c
@@ -56,18 +56,13 @@ s32 ssl_handle_flag(int argc, char **argv, t_flag_context *flag_ctx) {
 
 	ssl_opt_flag_init(argv[0], flag_ctx);
 
-	/* Need to adapt to to -1/+1 for skip first args (hash function argument)*/
-	// flag = parse_flag(argc, argv, flag_ctx, &error);
-	
-
-	
+	/* -1 +1 cause we skip first args (algo_mod) */
 	flag = parse_flag(argc - 1, argv + 1, flag_ctx, &error);
 	if (error == -1) {
 		ft_printf_fd(1, "Parse_flag error: %d\n", error);
 		return (-1);
 	}
 	display_option_list(*flag_ctx);
-	// *input_str = get_opt_value(flag_ctx->opt_lst, flag, S_OPTION);
 	return (flag);
 }
 
@@ -105,8 +100,104 @@ void free_hash_context(HashCtx *ctx) {
 
 }
 
+char *str_join_char(char *str, char c, char c2) {
+	char *ret = NULL;
+	t_sstring sstr = {};
+
+	clear_sstring(&sstr);
+	push_sstring(&sstr, c);
+	concat_sstring(&sstr, str);
+	if (c2 != '\0') {
+		push_sstring(&sstr, c2);
+	} else {
+		push_sstring(&sstr, c);
+	}
+	ret = ft_strdup(sstr.data);
+	return (ret);
+}
+
+char *str_add_char_before(char *str, char c) {
+	char *ret = NULL;
+	t_sstring sstr = {};
+
+	clear_sstring(&sstr);
+	push_sstring(&sstr, c);
+	concat_sstring(&sstr, str);
+	ret = ft_strdup(sstr.data);
+	return (ret);
+}
+
+char *str_push_char_free(char *str, char c) {
+	char *ret = NULL;
+	t_sstring sstr = {};
+
+	clear_sstring(&sstr);
+	concat_sstring(&sstr, str);
+	push_sstring(&sstr, c);
+	ret = ft_strdup(sstr.data);
+	free(str);
+	return (ret);
+}
+
+// typedef struct s_contex_info {
+// 	char *prefix;
+// 	char *suffix;
+// } ContextInfo;
+
+// ContextInfo *handle_contextual_info(char *algo_name, char *str, char *path, int is_stdin, int flag_val) {
+// 	ContextInfo *info = ft_calloc(sizeof(ContextInfo), 1);
+// 	char *quote_str = NULL;
+// 	char *braces_str = NULL;
+// 	char *braces_quote_str = NULL;
+
+// 	if (str) {
+// 		quote_str = str_join_char(str, '\"', '\0');
+// 		braces_quote_str = str_join_char(quote_str, '(', ')');
+// 		braces_str = str_join_char(path, '(', ')');
+// 	}
+
+//     if (!has_flag(flag_val, Q_OPTION)) {
+//         if (!has_flag(flag_val, R_OPTION)) {
+//             if (str && !is_stdin) {
+//                 // prefix = ft_sprintf("%s (\"%s\") = ", algo_name, str);
+// 				info->prefix = ft_strjoin_free(braces_quote_str, " = ", 'n');
+// 				info->prefix = str_add_char_before(info->prefix, ' ');
+// 				info->prefix = ft_strjoin_free(algo_name, info->prefix, 's');
+//             } else if (str && is_stdin) {
+//                 // info->prefix = ft_sprintf("(\"%s\") = ", str);
+// 				info->prefix = ft_strjoin_free(braces_quote_str, " = ", 'n');
+//             } else {
+//                 // info->prefix = ft_sprintf("%s (%s) = ", algo_name, path);
+// 				info->prefix = ft_strjoin_free(braces_str, " = ", 'n');
+// 				info->prefix = str_add_char_before(info->prefix, ' ');
+// 				info->prefix = ft_strjoin_free(algo_name, info->prefix, 's');
+// 			}
+//         } else {
+// 			info->suffix = str ? str_add_char_before(quote_str, ' ') : str_add_char_before(path, ' ');
+// 			info->suffix = str_push_char_free(info->suffix, '\n');
+//         }
+//     }
+
+// 	// ft_printf_fd(1, "prefix: %s\n", info->prefix);
+// 	// ft_printf_fd(1, "suffix: %s\n", info->suffix);
+
+// 	if (!info->prefix) {
+// 		info->prefix = ft_strdup("");
+// 	}
+
+// 	if (!info->suffix) {
+// 		info->suffix = ft_strdup("\n");
+// 	}
+
+
+// 	free(quote_str);
+// 	free(braces_str);
+// 	free(braces_quote_str);
+// 	return (info);
+// }
 
 void hash_process(HashCtx *ctx, char *path, char *str, s8 is_stdin) {
+	
 	if (str) {
 		ctx->hash_str_func(ctx, (u8 *)str, ft_strlen(str));
 	} else {
@@ -115,27 +206,40 @@ void hash_process(HashCtx *ctx, char *path, char *str, s8 is_stdin) {
 		}
 	}
 
+	/* Cut last \n if is stdin */
 	if (is_stdin) {
 		int len = ft_strlen(str);
 		if (len > 0 && str[len - 1] == '\n') {
 			str[len - 1] = '\0';
 		}
+		/* Quiet mode with stdin (-P) */
 		if (has_flag(ctx->flag_val, Q_OPTION)) {
 			ft_printf_fd(1, "%s\n", str);
 		}
 	}
 
+	// ContextInfo *info = handle_contextual_info(ctx->algo_name, str, path, is_stdin, ctx->flag_val);
+
+	/* Classic display */
 	if (!has_flag(ctx->flag_val, Q_OPTION) && !has_flag(ctx->flag_val, R_OPTION)) {
 		if (str && !is_stdin) {
 			ft_printf_fd(1, "%s (\"%s\") = ", ctx->algo_name, str);
 		} else if (str && is_stdin) {
-
 			ft_printf_fd(1, "(\"%s\") = ", str);
 		} else {
 			ft_printf_fd(1, "%s (%s) = ", ctx->algo_name, path);
 		}
 	}
+
+	// ft_printf_fd(1, "%s", info->prefix);
+	/* Display hash */
 	display_hash(ctx->hash, ctx->hash_size);
+	// ft_printf_fd(1, "%s", info->suffix);
+
+	// free(info->prefix);
+	// free(info->suffix);
+
+	/* Reverse display (display info data after hash)*/
 	if (!has_flag(ctx->flag_val, Q_OPTION) && has_flag(ctx->flag_val, R_OPTION)) {
 		if (str) {
 			ft_printf_fd(1, " \"%s\"\n", str);
@@ -159,7 +263,6 @@ void read_stdin(HashCtx *ctx) {
 		ctx->stdin_str = ft_strdup(content);
 		ctx->stdin_strlen = ft_strlen(content);
 		hash_process(ctx, NULL, ctx->stdin_str, TRUE);
-		// ctx->hash_str_func(ctx, (u8 *)ctx->stdin_str, ctx->stdin_strlen);
 	}
 }
 
@@ -183,27 +286,17 @@ int main(int argc, char **argv) {
 	if (has_flag(ctx.flag_val, S_OPTION)) {
 		ctx.input_str = get_opt_value(ctx.flag_ctx.opt_lst, ctx.flag_val, S_OPTION);
 		hash_process(&ctx, NULL, ctx.input_str, FALSE);
-		// ctx.input_strlen = ft_strlen(ctx.input_str);
-		// ctx.hash_str_func(&ctx, (u8 *)ctx.input_str, ctx.input_strlen);
 	}
 	
 	/* Extract file args in t_list */
 	ctx.input_file = extract_args(argc, argv);
-	/* process all other args to set input file lst */
-	// ft_lstadd_front(&ctx.input_file, ft_lstnew(ft_strdup("ft_ssl")));
 
+	/* process all file args */
 	for (t_list *tmp = ctx.input_file; tmp; tmp = tmp->next) {
 		hash_process(&ctx, (char *)tmp->content, NULL, FALSE);
 		// ctx.hash_file_func(&ctx, (char *)tmp->content);
 	}
 
 	free_hash_context(&ctx);
-
-	// ctx.hash_file_func(ctx.input_file->content);
-	// MD5_hash_str((u8 *)"abc", ft_strlen("abc"));
-	// MD5_hash_str((u8 *)TESTSTRING, ft_strlen(TESTSTRING));
-	// MD5_hash_file_read("Makefile");
-	// MD5_hash_file_read("src/main.c");
-	// MD5_hash_file("ft_ssl");
 	return (0);
 }
